784ad014fd4150afb21f89aafe35cc5970e5c99e
Victor <VictorCarmona9035@hotmail.com>
2022-10-23 19:04:30 +0200

    411-Configuraciones iniciales del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MapasModule } from './mapas/mapas.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MapasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
