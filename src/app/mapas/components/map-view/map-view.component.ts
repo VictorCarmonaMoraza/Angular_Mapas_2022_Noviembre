58c0021a809949cbe2353c18c3c6c3dd29de379a
Victor <VictorCarmona9035@hotmail.com>
2022-10-23 21:09:52 +0200

    414-Mostrar un mapa

--- src/app/mapas/components/map-view/map-view.component.ts
+++ src/app/mapas/components/map-view/map-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
 import { PlacesService } from '../../services/places.service';
 
 @Component({
@@ -6,12 +7,34 @@ import { PlacesService } from '../../services/places.service';
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
 
-  constructor(private placesService:PlacesService) { }
+  @ViewChild('mapDiv')
+  mapDivElement!: ElementRef;
 
-  ngOnInit(): void {
-    console.log(this.placesService.useLoaction);
+
+  constructor(private placesService: PlacesService) { }
+
+  //Despues que mi vista ya esta renderizada
+  ngAfterViewInit(): void {
+
+    //Sino tenemos location
+    if (!this.placesService.useLoaction) throw Error('No hay placesService.userLocation');
+
+    const map = new mapboxgl.Map({
+      container: this.mapDivElement.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: this.placesService.useLoaction, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      //projection: 'globe' // display the map as a 3D globe
+    });
+    map.on('style.load', () => {
+    map.setFog({}); // Set the default atmosphere style
+    });
   }
+
+  // ngOnInit(): void {
+  //   console.log(this.placesService.useLoaction);
+  // }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+Mapboxgl.accessToken = 'pk.eyJ1IjoidmljdG9ybTkwMzUiLCJhIjoiY2w3bG1ibmtnMGkzbDNvbzh5cmx2YjlvaSJ9.hBx-PlcLLwYOKMr1vopfgA';
+
 if(!navigator.geolocation){
   alert('Navegador no soporta la Geolocation');
   throw new Error('Navegador no soporta la Geolocation');
